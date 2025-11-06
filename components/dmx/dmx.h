49f848f8795df52fbc9c7884ec09f743d08b13a6
juniormajor <bernhard.hofbauer@gmail.com>
2025-11-06 17:52:33 +0100

    include

--- components/dmx/dmx.h
+++ components/dmx/dmx.h
@@ -1,19 +1,19 @@
 #pragma once
 #include "esphome/core/component.h"
 
-#include "../../src/esp_dmx.h"
-#include "../../src/dmx/sniffer.h"
-#include "../../src/dmx/include/device.h"
-#include "../../src/dmx/include/driver.h"
-#include "../../src/dmx/include/parameter.h"
-#include "../../src/dmx/include/service.h"
-#include "../../src/dmx/include/types.h"
-#include "../../src/dmx/hal/gpio.h"
-#include "../../src/dmx/hal/nvs.h"
-#include "../../src/dmx/hal/timer.h"
-#include "../../src/dmx/hal/uart.h"
-#include "../../src/rdm/controller.h"
-#include "../../src/rdm/responder.h"
+#include "/src/esp_dmx.h"
+#include "/src/dmx/sniffer.h"
+#include "/src/dmx/include/device.h"
+#include "/src/dmx/include/driver.h"
+#include "/src/dmx/include/parameter.h"
+#include "/src/dmx/include/service.h"
+#include "/src/dmx/include/types.h"
+#include "/src/dmx/hal/gpio.h"
+#include "/src/dmx/hal/nvs.h"
+#include "/src/dmx/hal/timer.h"
+#include "/src/dmx/hal/uart.h"
+#include "/src/rdm/controller.h"
+#include "/src/rdm/responder.h"
 
 namespace esphome {
 namespace dmx {
